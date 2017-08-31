011c89bf8036267a9046a06b95c52caf12b5ddf5
kuldipc-ct <kchakraborty@connecture.com>
2017-08-31 16:19:31 +0530

    Added skeleton classes

---
+++ source/src/com/insurance/domain/Customer.java
@@ -0,0 +1,5 @@
+package com.insurance.domain;
+
+public class Customer {
+
+}
---
+++ source/src/test/insurance/TestInsuranceQuoteGenerator.java
@@ -0,0 +1,14 @@
+package test.insurance;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestInsuranceQuoteGenerator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
